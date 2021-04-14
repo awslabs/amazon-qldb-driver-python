f6baaa62f6199516235e47bd8319a049efc73922
byronlin13 <57543366+byronlin13@users.noreply.github.com>
2021-04-13 20:59:16 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import setuptools
 
 ROOT = os.path.join(os.path.dirname(__file__), 'pyqldb')
 VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.a-z\-]+)['"]''')
-requires = ['amazon.ion>=0.7.0,<1',
+requires = ['amazon.ion>=0.3.1,<1',
             'boto3>=1.16.56,<2',
             'botocore>=1.19.56,<2',
             'ionhash>=1.1.0,<2']
